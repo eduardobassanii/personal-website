a594071e3d483b9b9f890195cd0b8077ef8b7da9
Eduardo Bassani <eduardobassani@w134-87-036-012.wireless.uvic.ca>
2023-02-26 10:37:38 -0800

    add files

---
+++ script.js
@@ -0,0 +1,14 @@
+function showSoccerInfo() {
+    document.getElementById("soccer-button").style.display = "none";
+    document.getElementById("soccer-information").style.display = "block"
+}
+
+function showVideoGamesInfo() {
+    document.getElementById("video-games-button").style.display = "none";
+    document.getElementById("video-games-information").style.display = "block"
+}
+
+function showSurfInfo() {
+    document.getElementById("surf-button").style.display = "none";
+    document.getElementById("surf-information").style.display = "block"
+}
